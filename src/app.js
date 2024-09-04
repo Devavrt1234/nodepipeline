6d8fc38550d8e5cb446e3100264e37875b64f0b3
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-04 11:46:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Chaitanya Sharma');
+  res.send('DEVAVRAT SINGH');
 });
 
 module.exports = app;
